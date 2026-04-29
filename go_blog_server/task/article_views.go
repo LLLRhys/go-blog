bef7776bd093a529e11780a28c592f42adbea34b
Rhys <2528765108@qq.com>
2026-04-29 21:03:06 +0800

    1. 简化了关于文章阅读量的定时任务的错误处理。若es更新报错，就return err，停 止更新（阅读量都没变化 因为本次更新也失败了，同时也没有清空redis中的数据，数 据不会出错）。定时任务还会继续，直到可以更新为止。

--- go_blog_server/task/article_views.go
+++ go_blog_server/task/article_views.go
@@ -2,7 +2,6 @@ package task
 
 import (
 	"context"
-	"errors"
 	"server/global"
 	"server/model/elasticsearch"
 	"server/service"
@@ -19,7 +18,6 @@ func UpdateArticleViewsSyncTask() error {
 	articleView := service.ServiceGroupApp.ArticleService.NewArticleView()
 
 	viewsInfo := articleView.GetInfo()
-	hasError := false
 	for id, num := range viewsInfo {
 		// 无变化就跳过
 		if num == 0 {
@@ -32,17 +30,10 @@ func UpdateArticleViewsSyncTask() error {
 		_, err := global.ESClient.Update(elasticsearch.ArticleIndex(), id).Script(&script).Do(context.TODO())
 		if err != nil {
 			global.Log.Error("Failed to update article views", zap.String("article_id", id), zap.Error(err))
-			hasError = true
+			return err
 		}
 	}
 
-	// 只在全部成功时才清除 Redis 数据，避免数据重复累加
-	if !hasError {
-		articleView.Clear()
-	}
-
-	if hasError {
-		return errors.New("some articles failed to update views")
-	}
+	articleView.Clear()
 	return nil
 }
